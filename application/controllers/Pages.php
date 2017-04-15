9214037d5aacabbc226fa13896633cd727ae8935
Alex Neises <alex.neises@purplewave.com>
2017-04-14 19:39:55 -0500

    Added versioning to footer.

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -13,6 +13,10 @@ class Pages extends CI_Controller
 		$data['top_text'] = 'Kansas Knights Convention';
 		$data['bottom_text'] = '2019';
 		$data['news_item'] = $this->news_model->get_news();
+		$readme = file_get_contents('README.md');
+		$secondhalf = explode('## Version ', $readme);
+		$version = explode("\n\n### CHANGELOG", $secondhalf[1]);
+		$data['version'] = $version[0];
 
 		$this->load->view('templates/header', $data);
 		$this->load->view('pages/' . $page, $data);
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,14 @@
 				</div>
 			</div>
 			<footer>
-				<em>&copy; <?php echo date('Y'); ?> Knights of Columbus - Kansas State Council. All rights reserved.</em>
+				<div class="row">
+					<div class="col-lg-6">
+						<em>&copy; <?php echo date('Y'); ?> Knights of Columbus - Kansas State Council. All rights reserved.</em>
+					</div>
+					<div class="right col-lg-6">
+						<em>Version <?php echo $version; ?></em>
+					</div>
+				</div>
 				<script src="<?php echo site_url('static/jquery/jquery-3.1.1.min.js'); ?>"></script>
 				<script src="<?php echo site_url('static/tether/js/tether.min.js'); ?>"></script>
 				<script src="<?php echo site_url('static/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
